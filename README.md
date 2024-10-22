d8ae983dc9a2684d2c8898411de63910c7a6e6f6
Yevhenii Blynnyi <yevhenii.blynnyi@gen.tech>
2024-10-22 17:26:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Preconditions:
+- #### To run the project use an emulator or real device
+- Download the project
+- Configure config.properties file with appropriate values
+- BaseTest.class already configured to use Appium
+- Install Appium 2.0, then install uiautomator2 driver
+  - `appium driver install uiautomator2`
+
+## Test Task:
+- Write a test for registration (onboarding) with checks.
+- The test starts by pressing the "GET STARTED" button.
+
+## While reviewing the test task we'll focus on:
+- [x] Framework is described at least at a basic level.
+- [x] The test should comprehensively cover the necessary functionality.
+- [x] The code should be clean, well-structured, and easy to read.
+- [x] The test must include sufficient checks (assertions).
+- [x] The test runs successfully.
+- [x] No code duplication.
+- [x] The code should avoid common anti-patterns such as hard-coded values, over-complicated logic, or inefficient structures.
---
+++ src/test/java/co/obrio/BaseTest.java
@@ -0,0 +1,85 @@
+package co.obrio;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.Properties;
+
+public class BaseTest {
+
+    private static Properties properties = null;
+    private static UiAutomator2Options capabilities = null;
+
+    protected AndroidDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        setProperties();
+        setCapability();
+        setDriver();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if(driver != null) {
+            driver.quit();
+        }
+    }
+
+    private void setProperties() {
+        if (properties == null) {
+            try {
+                properties = new Properties();
+                FileInputStream fis = new FileInputStream(getPathWithCorrectSeparator("src/test/resources/config.properties"));
+                properties.load(fis);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void setDriver() {
+        try {
+            driver = new AndroidDriver(
+                    new URL(properties.getProperty("appium.server.url")),
+                    capabilities
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setCapability() {
+        if (capabilities == null) {
+            capabilities = new UiAutomator2Options();
+            capabilities.setCapability("platformName", properties.getProperty("platform.name"));
+            capabilities.setCapability("platformVersion", properties.getProperty("platform.version"));
+            capabilities.setCapability("deviceName", properties.getProperty("device.name"));
+            capabilities.setCapability("automationName", properties.getProperty("automationName"));
+            capabilities.setCapability("app", getAppAbsolutePath());
+        }
+    }
+
+    private String getAppAbsolutePath() {
+        String appName = properties.getProperty("app.name");
+        URL resource = getClass().getClassLoader().getResource(appName);
+        if (resource == null) {
+            throw new RuntimeException("App not found " + appName + " in resource directory");
+        }
+        try {
+            return new File(resource.toURI()).getAbsolutePath();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Something went wrong for app: " + appName);
+        }
+    }
+
+    private String getPathWithCorrectSeparator(String path) {
+        return path.replaceAll("/", File.separator);
+    }
+}
